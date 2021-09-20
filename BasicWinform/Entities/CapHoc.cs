ffe2ae0c4ce9129813186b05c03d6330ad35dfb2
JTC-IT <trungchinhjuse@gmail.com>
2021-09-20 20:54:57 +0700

    Add Entities

---
+++ BasicWinform/Entities/CapHoc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWinform.Entities
+{
+    public class CapHoc
+    {
+        public string Cap { get; set; }
+        public string School { get; set; }
+        public float Diem { get; set; }
+        public HK HanhKiem { get; set; }
+
+        public enum HK { Tot, Kha, TB, Yeu }
+
+        public string HanhKiemToString
+        {
+            get
+            {
+                if (HanhKiem == HK.Tot)
+                    return "Tốt";
+                else if (HanhKiem == HK.Kha)
+                    return "Khá";
+                else if (HanhKiem == HK.TB)
+                    return "Trung bình";
+                else
+                    return "Yếu";
+            }
+        }
+    }
+}
---
+++ BasicWinform/Entities/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWinform.Entities
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public Sex sex { get; set; }
+        public DateTime DOB { get; set; }
+        public string homeTown { get; set; }
+
+        public enum Sex { Nam, Nu, Khac };
+
+        public string fullName
+        {
+            get { return lastName + " " + firstName; }
+        }
+
+        public CapHoc Cap1 { get; set; }
+        public CapHoc Cap2 { get; set; }
+        public CapHoc Cap3 { get; set; }
+    }
+}
